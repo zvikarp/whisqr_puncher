7534c3270cad47bbf4843fd83d8bb8faace880e8
zvikarp <zvi@zektec.com>
2021-03-25 01:06:10 +0200

    🚧 added signout  button

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -7,16 +7,18 @@ import 'package:whisqr_puncher/models/user.dart';
 class StorageUtil {
   final _storage = FlutterSecureStorage();
 
-  Future<String> get(String key) async => await _storage.read(key: key);
+  Future<String> _get(String key) async => await _storage.read(key: key);
 
-  Future<void> set(String key, String value) async =>
+  Future<void> _set(String key, String value) async =>
       await _storage.write(key: key, value: value);
 
   Future<User> getUser() async => User.fromMap(Map<String, dynamic>.from(
-      jsonDecode((await get(GeneralConsts.STORAGE_USER) ?? '{}'))));
+      jsonDecode((await _get(GeneralConsts.STORAGE_USER) ?? '{}'))));
 
   Future<void> setUser(User user) async =>
-      set(GeneralConsts.STORAGE_USER, jsonEncode(user.toMap()));
+      _set(GeneralConsts.STORAGE_USER, jsonEncode(user.toMap()));
+
+  Future<void> deleteUser() async => await _storage.deleteAll();
 }
 
 final StorageUtil storageUtil = StorageUtil();
--- lib/widgets/scanner/header.dart
+++ lib/widgets/scanner/header.dart
@@ -1,10 +1,18 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_text_helpers/flutter_text_helpers.dart';
 
 import 'package:whisqr_puncher/consts/theme.dart';
 import 'package:whisqr_puncher/utils/l18n.dart';
+import 'package:whisqr_puncher/utils/router.gr.dart';
+import 'package:whisqr_puncher/utils/storage.dart';
 
 class ScannerHeaderWidget extends StatelessWidget {
+  Future<void> _onTapSignout(BuildContext context) async {
+    await storageUtil.deleteUser();
+    AutoRouter.of(context).replace(SpalshScreenRoute());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -12,9 +20,16 @@ class ScannerHeaderWidget extends StatelessWidget {
         horizontal: ThemeConsts.XXL_PAD,
         vertical: ThemeConsts.XL_PAD,
       ),
-      child: HeadlineText1(
-        l18nUtil.t('screen.scanner.header'),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          HeadlineText1(
+            l18nUtil.t('screen.scanner.header'),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+              onPressed: () => _onTapSignout(context),
+              child: BodyText2(l18nUtil.t('screen.scanner.signout-btn')))
+        ],
       ),
     );
   }
