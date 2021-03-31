9ad3174f2fe9070f8038e71a7435292ad083233e
zvikarp <zvi@zektec.com>
2021-03-31 13:10:49 +0300

    🔧 added mobx

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:provider/provider.dart';
 
 import 'package:whisqr_puncher/consts/general.dart';
+import 'package:whisqr_puncher/stores/business.dart';
 import 'package:whisqr_puncher/utils/l18n.dart';
 import 'package:whisqr_puncher/utils/router.gr.dart';
 import 'package:whisqr_puncher/utils/theme.dart';
@@ -10,6 +12,12 @@ import 'package:whisqr_puncher/utils/theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await l18nUtil.init();
+  runApp(
+    MultiProvider(providers: [
+      Provider<BusinessStore>(create: (_) => BusinessStore()),
+      // Provider<CustomerStore>(create: (_) => CustomerStore()),
+    ], child: App()),
+  );
   runApp(App());
 }
 
